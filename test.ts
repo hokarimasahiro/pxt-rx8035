f624d18db99deaf2ad63257a10cf56ffd1511536
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-06-18 03:14:44 +0900

    update

--- test.ts
+++ test.ts
@@ -1,5 +1,10 @@
 // tests go here; this will not be compiled when this package is used as a library
 basic.forever(function () {
-    let buf = rx8035.getData
-    basic.showNumber(123)
+    let buf = rx8035.getData()
+    for(let i=0;i<16;i++){
+        basic.clearScreen()
+        basic.pause(200)
+        basic.showNumber(buf[i])
+        basic.pause(300)
+    }
 })
