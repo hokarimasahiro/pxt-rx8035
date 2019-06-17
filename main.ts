abfa142e3443c3fa6a06a03d2a6de401d544804d
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-06-18 04:08:07 +0900

    update

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@
 /**
  * rx8035 block
  */
-//% weight=10 color=#800000 icon="\uf017" block="rx8035"
+//% weight=10 color=#800080 icon="\uf017" block="rx8035"
 namespace rx8035 {
     let I2C_ADDR = 0x32
     let REG_CTRL1 = 0x0e
@@ -51,159 +51,6 @@ namespace rx8035 {
     }
 
     /**
-     * get Year
-     */
-    //% blockId="getYear" block="get year"
-    //% weight=100 blockGap=8
-    export function getYear(): number {
-        return (HexToDec(getReg(REG_YEAR)))
-    }
-
-    /**
-     * set year
-     * @param dat data of year, eg: 2019
-     */
-    //% blockId="setYear" block="set year %dat"
-    //% weight=99 blockGap=8
-    export function setYear(dat: number): void {
-        setReg(REG_YEAR, DecToHex(dat % 100))
-    }
-
-    /**
-     * get month
-     */
-    //% blockId="getmMonth" block="get month"
-    //% weight=98 blockGap=8
-    export function getMonth(): number {
-        return HexToDec(getReg(REG_MONTH) & 0x1f)
-    }
-
-    /**
-     * set month
-     * @param dat data of month, eg: 3
-     */
-    //% blockId="setMonth" block="set month %dat"
-    //% weight=97 blockGap=8
-    export function setMonth(dat: number): void {
-        setReg(REG_MONTH, DecToHex(dat % 13))
-    }
-
-    /**
-     * get day
-     */
-    //% blockId="getDay" block="get day"
-    //% weight=96 blockGap=8
-    export function getDay(): number {
-        return HexToDec(getReg(REG_DAY) & 0x3f)
-    }
-
-    /**
-     * set day
-     * @param dat data of day, eg: 14
-     */
-    //% blockId="setDay" block="set day %dat"
-    //% weight=95 blockGap=8
-    export function setDay(dat: number): void {
-        setReg(REG_DAY, DecToHex(dat % 32))
-    }
-
-    /**
-     * get weekday
-     */
-    //% blockId="getWeekday" block="get weekday"
-    //% weight=94 blockGap=8
-    export function getWeekday(): number {
-        return HexToDec(getReg(REG_WEEKDAY) & 0x07)
-    }
-
-    /**
-     * set weekday
-     * @param dat data of weekday, eg: 4
-     */
-    //% blockId="setWeekday" block="set weekday %dat"
-    //% weight=93 blockGap=8
-    export function setWeekday(dat: number): void {
-        setReg(REG_WEEKDAY, DecToHex(dat % 7) | 0x18)
-    }
-
-    /**
-     * get hour
-     */
-    //% blockId="getHour" block="get hour"
-    //% weight=92 blockGap=8
-    export function getHour(): number {
-        return HexToDec(getReg(REG_HOUR) & 0x3f)
-    }
-
-    /**
-     * set hour
-     * @param dat data of hour, eg: 5
-     */
-    //% blockId="setHour" block="set hour %dat"
-    //% weight=91 blockGap=8
-    export function setHour(dat: number): void {
-        setReg(REG_HOUR, DecToHex(dat % 24))
-    }
-
-    /**
-     * get Minute
-     */
-    //% blockId="getMinute" block="get minute"
-    //% weight=90 blockGap=8
-    export function getMinute(): number {
-        return HexToDec(getReg(REG_MINUTE) & 0x7f)
-    }
-
-    /**
-     * set minute
-     * @param dat data of minute, eg: 30
-     */
-    //% blockId="setMinute" block="set minute %dat"
-    //% weight=89 blockGap=8
-    export function setMinute(dat: number): void {
-        setReg(REG_MINUTE, DecToHex(dat % 60))
-    }
-
-    /**
-     * get Second
-     */
-    //% blockId="getseconde" block="get second"
-    //% weight=88 blockGap=8
-    export function getSecond(): number {
-        return HexToDec(getReg(REG_SECOND) & 0x7f)
-    }
-
-    /**
-     * set second
-     * @param dat data of second, eg: 0
-     */
-    //% blockId="setSecond" block="set second %dat"
-    //% weight=87 blockGap=8
-    export function setSecond(dat: number): void {
-        setReg(REG_SECOND, DecToHex(dat % 60) | 0x80)
-    }
-
-    /**
-     * get Xstp
-     */
-    //% blockId="isXstp" block="test Xstp"
-    //% weight=86 blockGap=8
-    export function isXstp(): boolean {
-        if ((getReg(REG_CTRL2) & 0x20) != 0) return true;
-        else return false;
-    }
-
-    /**
-     * reset Xstp
-     */
-    //% blockId="resetXstp" block="reset Xstp"
-    //% weight=85 blockGap=8
-    export function resetXstp(): void {
-        setReg(REG_CTRL1, 0x00)
-        setReg(REG_CTRL2, 0x00)
-    }
-
-    /**
      * set Date and Time
      * @param year data of year, eg: 2019
      * @param month data of month, eg: 3
@@ -229,28 +76,21 @@ namespace rx8035 {
         pins.i2cWriteBuffer(I2C_ADDR, buf)
     }
     /**
-     * set RTC
-     * @param ad address of RTC, eg: 0
-     * @param dat data of RTC, eg: 0xaa
+     * get RTC DATA
      */
-    //% blockId="setRTC" block="set RTC address=%ad data=%dat"
-    //% weight=50 blockGap=8
-    export function setRTC(ad: number, dat: number): void {
-        let buf = pins.createBuffer(2);
-        buf[0] = ad;
-        buf[1] = dat;
-        pins.i2cWriteBuffer(I2C_ADDR, buf);
-    }
-
-    /**
-     * get RTC
-     * @param ad address of RTC, eg: 0
-     */
-    //% blockId="getRTC" block="get RTC address=%ad"
-    //% weight=48 blockGap=8
-    export function getRTC(ad: number): number {
-        pins.i2cWriteNumber(I2C_ADDR, ad, NumberFormat.UInt8BE);
-        return pins.i2cReadNumber(I2C_ADDR, NumberFormat.UInt8BE);
+    //% blockId="getData" block="get RTC data"
+    //% weight=68 blockGap=8
+    export function getData(): number[] {
+        let retbuf = [0, 0, 0, 0, 0, 0, 0]
+        let buf = getRawData();
+        retbuf[0] = HexToDec(buf[7])        // year
+        retbuf[1] = HexToDec(buf[6] & 0x1f) // month
+        retbuf[2] = HexToDec(buf[5] & 0x3f) // day
+        retbuf[3] = HexToDec(buf[4] & 0x07) // weekday
+        retbuf[4] = HexToDec(buf[3] & 0x3f) // hour
+        retbuf[5] = HexToDec(buf[2] & 0x7f) // minute
+        retbuf[6] = HexToDec(buf[1] & 0x7f) // second
+        return retbuf;
     }
     /**
      * get RTC RAW DATA
@@ -263,23 +103,6 @@ namespace rx8035 {
         for (let i = 0; i < 16; i++) {
             retbuf[i] = buf[i]
         }
-        return retbuf;
-    }
-    /**
-     * get RTC DATA
-     */
-    //% blockId="getData" block="get RTC data"
-    //% weight=46 blockGap=8
-    export function getData(): number[] {
-        let retbuf = [0, 0, 0, 0, 0, 0, 0]
-        let buf = getRawData();
-        retbuf[0] = HexToDec(buf[7])        // year
-        retbuf[1] = HexToDec(buf[6] & 0x1f) // month
-        retbuf[2] = HexToDec(buf[5] & 0x3f) // day
-        retbuf[3] = HexToDec(buf[4] & 0x07) // weekday
-        retbuf[4] = HexToDec(buf[3] & 0x3f) // hour
-        retbuf[5] = HexToDec(buf[2] & 0x7f) // minute
-        retbuf[6] = HexToDec(buf[1] & 0x7f) // second
         return retbuf;
     }
 }
