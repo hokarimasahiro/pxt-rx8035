178037ab662b905fb46f888b10b56fbc7e689d0f
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2019-06-18 03:12:09 +0900

    update

--- test.ts
+++ test.ts
@@ -1,13 +1,4 @@
 // tests go here; this will not be compiled when this package is used as a library
 basic.forever(function () {
-    basic.clearScreen()
-    let buf = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 9, 8, 7, 6, 5, 4, 3]
-    for (let i = 0; i < 16; i++) {
-        basic.clearScreen()
-        basic.pause(500)
-        basic.showNumber(buf(i))
-        basic.pause(500)
-    }
-    basic.clearScreen()
-    basic.pause(1000)
+    basic.showNumber(123)
 })
